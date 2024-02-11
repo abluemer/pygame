30736931c35fe4f732b550efd4060f3c8343dc4f
Hakan <hakan.bulut@gmx.net>
2024-02-12 00:07:01 +0100

    asd1

--- spieler.py
+++ spieler.py
@@ -11,6 +11,7 @@ class Player:
         self.screen_height = screen_height
         self.speed = 5
 
+
     def move(self, keys):
         if keys[pygame.K_LEFT] and self.x > 0:
             self.x -= self.speed
@@ -22,8 +23,11 @@ class Player:
             self.y += self.speed
 
     def draw(self, window):
-       # pygame.draw.rect(window, (0, 0, 250), (self.x, self.y, self.width, self.height))
-        player_img = pygame.image.load("player.png").convert()
-        player_img = pygame.transform.scale(player_img, (30, 50))
+        pygame.image.load("player.png").convert()
+        screen.blit(player_img, player_rect)
+
+        #rect(window, (0, 0, 255), (self.x, self.y, self.width, self.height))
+        #player_img = pygame.image.load("player.png").convert()
+       # player_img = pygame.transform.scale(player_img, (30, 50))
     def check_collision(self, other):
         return pygame.Rect(self.x, self.y, self.width, self.height).colliderect(other.x, other.y, other.width, other.height)
